99ff96dec73f03e9889d4b73f835ef2e87bb60cc
Kyle Friel <businessfriel@gmail.com>
2021-06-28 13:18:48 -0400

    Project directories setup

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TopNav from "./TopNav/TopNav";
+
+const LandingPage = () => {
+    return (
+        <>
+            <TopNav />
+            <div>Landing Page</div>
+        </>
+    );
+};
+
+export default LandingPage;
