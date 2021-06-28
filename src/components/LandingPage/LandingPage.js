2e7e8f25789eb92bf6e401ff278e02c83fc788fd
Kyle Friel <businessfriel@gmail.com>
2021-06-28 13:56:56 -0400

    Added logo and favicon

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TopNav from "./TopNav/TopNav";
+import logo from "../../assets/images/logo.png";
 
 const LandingPage = () => {
     return (
         <>
             <TopNav />
             <div>Landing Page</div>
+            <img className="logo" src={logo} alt="logo" />
         </>
     );
 };
