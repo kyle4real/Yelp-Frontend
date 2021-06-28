db03f35d1104f6f9f524c362f650136c74f80222
Kyle Friel <businessfriel@gmail.com>
2021-06-28 13:33:07 -0400

    Setup Landing Page & installed Bulma css

--- src/components/LandingPage/TopNav/TopNav.js
+++ src/components/LandingPage/TopNav/TopNav.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const TopNav = () => {
-    return <div>Top Nav</div>;
+    return (
+        <div className="nav">
+            <div className="nav__links-left">
+                <span>Write a Review</span>
+                <span>Events</span>
+            </div>
+            <div className="nav__links-right">
+                <span>Login</span>
+                <button className="button">Sign up</button>
+            </div>
+        </div>
+    );
 };
 
 export default TopNav;
