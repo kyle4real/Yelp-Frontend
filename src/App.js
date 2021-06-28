7f48eeacce9576460ea5aaf228bb3d3c81611588
Kyle Friel <businessfriel@gmail.com>
2021-06-28 15:56:20 -0400

    add react router. add margin bottom to landing page search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Switch, Route } from "react-router-dom";
+
 import LandingPage from "./components/LandingPage/LandingPage";
+import Search from "./components/Search/Search";
 
 function App() {
     return (
-        <>
-            <LandingPage />
-        </>
+        <Switch>
+            <Route path="/search" component={Search} />
+            <Route path="/" component={LandingPage} />
+        </Switch>
     );
 }
 
