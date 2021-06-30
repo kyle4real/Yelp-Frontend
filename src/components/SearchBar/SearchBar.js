da8c8a7b9aee268109ce0afe28c7ecb0c1d94063
Kyle Friel <businessfriel@gmail.com>
2021-06-30 11:09:06 -0400

    Use State hooks in Search Bar component for search elements.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,39 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchBar = ({ small }) => {
+const SearchBar = ({ small, searchTerm, searchLocation }) => {
+    const [term, setTerm] = useState(searchTerm || "");
+    const [location, setLocation] = useState(searchLocation || "");
+
     const handleOnSubmit = (e) => {
         e.preventDefault();
-        console.log("submitted");
+        console.log(term, location);
     };
 
     const sizeClass = small ? "" : "is-medium";
     return (
         <div className="search-bar">
             <form className="field has-addons" onSubmit={handleOnSubmit}>
-                <p className="control">
-                    <a href="/" className={`button is-static ${sizeClass}`}>
-                        Search
-                    </a>
-                </p>
-                <p className="control">
+                <div className="control">
+                    <div className={`button is-static ${sizeClass}`}>Search</div>
+                </div>
+                <div className="control">
                     <input
                         className={`input input-control ${sizeClass}`}
+                        onChange={(e) => setTerm(e.target.value)}
                         type="text"
                         placeholder="burgers, barbers, spas, handymen"
                     />
-                </p>
-                <p className="control">
-                    <a href="/" className={`button is-static ${sizeClass}`}>
-                        Near
-                    </a>
-                </p>
-                <p className="control">
+                </div>
+                <div className="control">
+                    <div className={`button is-static ${sizeClass}`}>Near</div>
+                </div>
+                <div className="control">
                     <input
                         className={`input input-control ${sizeClass}`}
+                        onChange={(e) => setLocation(e.target.value)}
                         type="text"
                         placeholder="Where"
                     />
-                </p>
+                </div>
                 <button className={`button search-button ${sizeClass}`} onClick={handleOnSubmit}>
                     <span className="icon is-small">
                         <i className="fas fa-search search-button__icon"></i>
