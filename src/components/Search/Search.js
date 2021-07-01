24d6700eb44ed838061aa5498d653a04383daf52
Kyle Friel <businessfriel@gmail.com>
2021-07-01 11:14:05 -0400

    Yelp data request not working

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,12 +4,17 @@ import SubNav from "../NavBar/SubNav/SubNav";
 import SearchResultsSummary from "./SearchResultsSummary/SearchResultsSummary";
 import SearchResults from "./SearchResults/SearchResults";
 import useRouter from "use-react-router";
+import useBusinessSearch from "../../hooks/yelp-api/useBusinessSearch";
 
 const Search = () => {
     const { location } = useRouter();
     const params = new URLSearchParams(location.search);
     const searchTerm = params.get("find_desc");
     const searchLocation = params.get("find_loc");
+    const [businesses, amountResults, searchParams, setSearchParams] = useBusinessSearch(
+        searchTerm,
+        searchLocation
+    );
 
     return (
         <div>
